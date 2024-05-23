f1e25bc9c85274b0d9f0c1baae321cc013967ee0
UA <ugurayaz23@gmail.com>
2024-05-24 00:08:42 +0200

    initial commit

---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactNode } from 'react'
+
+const client = new QueryClient()
+
+const Providers = ({ children }: { children: ReactNode }) => {
+    return <QueryClientProvider client={client}>
+        {children}
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+}
+
+export default Providers
